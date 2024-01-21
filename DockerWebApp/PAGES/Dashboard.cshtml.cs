3db8099ea98074e071e322c6fcc474f541ed3a9e
kavita89834 <kumarikavita89834@gmail.com>
2024-01-21 18:58:36 +0530

    Initial commit

---
+++ DockerWebApp/PAGES/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DockerWebApp.PAGES
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
